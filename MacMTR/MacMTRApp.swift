be3881a65be85bde5ddefb0dce8883249e834b97
RetroWrangler <Retrowrangler@homemail.com>
2025-08-19 19:37:06 -0400

    Initial Commit

---
+++ MacMTR/MacMTRApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct MacMTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView() // Launch the new columnar UI
+        }
+    }
+}
---
+++ MacMTRTests/MacMTRTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacMTRTests.swift
+//  MacMTRTests
+//
+//  Created by Cory on 8/15/25.
+//
+
+import Testing
+@testable import MacMTR
+
+struct MacMTRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
